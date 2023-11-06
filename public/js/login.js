f17ad320d5148764bd08c76ee8518222ff53ec7a
annikacrossley <132411887+annikacrossley@users.noreply.github.com>
2023-11-05 21:13:39 -0600

    inital code for login, logout, and profile JS done

---
+++ public/js/login.js
@@ -0,0 +1,50 @@
+const loginFormHandler = async(event) => {
+    event.preventDefault();
+
+    //login
+    const username = document.querySelector('#user-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#name-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (name && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ name, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
